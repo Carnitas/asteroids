74e19d048fe205af33e166264fd44ae970236c1a
Carnitas <mhayes37@proton.me>
2025-05-07 07:09:54 -0600

    Asteroid outline building

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
-from constants import *
 
-def main():
-    pygame.init()
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main() -> None:
+    pygame.display.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.display.QUIT:
+                return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
