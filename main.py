035fae80ad3eda9c91a81de2c4862a22c0eaadc3
Carnitas <mhayes37@proton.me>
2025-05-07 06:54:17 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+
+
+if __name__ == "__main__":
+    main()
